2235e4aaccc0bc2cc79a05a92fed43bb6525680f
Jahid Hasan <slniloy500@gmail.com>
2021-02-03 10:28:40 +0600

    True VS False

---
+++ true-false.js
@@ -0,0 +1,28 @@
+// let age = 4;
+
+// if(age > 0){
+//     console.log("Condition is true");
+// }else{
+//     console.log("Condition is false");
+// }
+
+/*False Value
+0,
+"",
+NAN,
+null,
+undefined
+
+Truth Value
+1 or -1,
+" " string with white space or value
+
+*/
+let name = "jahid";
+
+if(name){
+    console.log("Condition is true");
+}else{
+    console.log("Condition is false");
+}
+
