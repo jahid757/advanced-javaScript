1a9dab3b187b3dd2ebbfc950e5912cc273ff2dd5
Jahid Hasan <slniloy500@gmail.com>
2021-02-03 10:41:23 +0600

    Undefined VS null

---
+++ undefined-null.js
@@ -0,0 +1,11 @@
+let newName ;
+// console.log(newName);
+
+function add(num1,num2){
+    // console.log(num1+num2);
+    // return 
+}
+// console.log(add(10,15));
+
+let nam = undefined;
+console.log(nam);
