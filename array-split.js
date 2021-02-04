95ab648350ccd6f932ee8fce13859ff19cf3f597
Jahid Hasan <slniloy500@gmail.com>
2021-02-04 22:26:21 +0600

    Advanced JavaScript

---
+++ array-split.js
@@ -0,0 +1,11 @@
+const number = [1,2,3,4,5,6,7,8,9];
+
+// const part = number.slice(0,number.length-1);
+// console.log(part);
+
+// const remove = number.splice(2,5);
+// console.log(remove);
+// console.log(number);
+
+const join = number.join(" From Join ");
+console.log(join);
---
+++ break-continue.js
@@ -0,0 +1,17 @@
+// const num = [1,2,3,4,5,6,7,8,9];
+// for (let i = 0; i < num.length; i++) {
+//     const element = num[i];
+//     if (element > 5) {
+//         break
+//     }
+//     console.log(element);
+// }
+
+const num = [1,-2,3,-4,5,6,-7,8,9];
+for (let i = 0; i < num.length; i++) {
+    const element = num[i];
+    if (element < 0) {
+        continue
+    }
+    console.log(element);
+}
